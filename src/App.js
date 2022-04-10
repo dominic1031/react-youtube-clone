4b6ec5f9ed8cb1c176da70e2b589007be46a1bb0
dominic1031 <dmnguyen15@uh.edu>
2022-04-10 05:13:38 -0500

    header basic styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Icons from './components/Icons/icons'
+import './App.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="masthead-container">
+                <div className="masthead">
+                    <div className="masthead--start">
+                        <img src={Icons.Hamburger} alt="" />
+                        <img src={Icons.LogoTitle} alt="" />
+                    </div>
+                    <div className="masthead--center">
+                        <div className="search-container">
+                            <form className="search-box">
+                                <img src={Icons.Search} alt="" />
+                                <input placeholder="Search" />
+                                <img src={Icons.ClearSearch} alt="" />
+                            </form>
+                            <button>
+                                <img src={Icons.Search} />
+                            </button>
+                        </div>
+                        <img
+                            className="voice-search"
+                            src={Icons.VoiceSearch}
+                            alt=""
+                        />
+                    </div>
+                    <div className="masthead--end">
+                        <div className="masthead--end--gap"></div>
+                        <div className="masthead--end--buttons">
+                            <div className="settings">
+                                <img src={Icons.Settings} alt="" />
+                            </div>
+                            <button className="sign-in">
+                                <img src={Icons.SignIn} alt="" />
+                                SIGN IN
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="contents">
+
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Icons/icons.js
@@ -0,0 +1,20 @@
+import Hamburger from '../../assets/images/svg8.svg'
+import LogoTitle from '../../assets/images/svg9.svg'
+import Logo from '../../assets/images/svg35.svg'
+import Search from '../../assets/images/svg10.svg'
+import ClearSearch from '../../assets/images/svg5.svg'
+import VoiceSearch from '../../assets/images/svg11.svg'
+import Settings from '../../assets/images/svg24.svg'
+import SignIn from '../../assets/images/YouTube-14.svg'
+const Icons = {
+    Hamburger,
+    LogoTitle,
+    Logo,
+    Search,
+    ClearSearch,
+    VoiceSearch,
+    Settings,
+    SignIn,
+}
+
+export default Icons
